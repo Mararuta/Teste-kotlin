9116d935b95c74313767f6efce1b11a7a02fbb3f
Marcelo Araujo <marcelocompf@gmail.com>
2022-02-21 11:17:42 -0300

    Primeiro exercício IntellJ

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(){
+       /*println("Olá, mundo!!!  ")*/
+    println("Alfredinho: Bom dia, galera.")
+    println("Marcelinho: Iae jovens, bom dia!")
+    println("Marininha: Bom dia xofens, com vão?")
+    println("Geovaninha: Bom dia, povo apareci aqui do nada")
+    println("Marininha: Como foi o fim de semana de vocês?")
+    println("Marcelinho: Meu foi tranquilo bem típico, trabalhando")
+    println("Alfredinho: Eu fiquei trampando e montando um layout do PI")
+    println("Marininha: Eu sai com uma friend e fiquei jogando")
+    println("Geovaninha: preparei minha apresentação pra hoje")
+    println("Marcelinho: Fim de semana produtivo é assim, aprendam")
+    println("Marininha: Eu resolvi não fazer muita coisa ness eFim de semana")
+    println("Alfredinho: eu queria ter ido comer queijo de cabra, mas nem consegui.")
+    println("Marcelinho: nooooossa, eu quase que consegui esses dias, " +
+            "mas o segurança do mercado me pegou antes")
+    println("Geovaninha: Verdade em, eu era o segurança do mercado")
+    println("Marininha: e eu era o queijo.")
+    println("Alfredinho: kkkkkkkkkkkkkkk")
+    println("Geovaninha: kkkkkkkkkkkkkkk")
+    println("Marininha: kkkkkkkkkkkkkkk")
+    println("Marcelinho: kkkkkkkkkkkkkk")
+}
